8dec477e9c51da1b1c4dc2b97d28b5b242bd5b6b
jjhannes <jj.hannes.swanepoel@gmail.com>
2024-03-16 09:55:58 +0200

    Initial commit - Solution and domain project - Provided account service interface

---
+++ RokkitBank.Domain/IAccountService.cs
@@ -0,0 +1,16 @@
+﻿
+namespace RokkitBank.Domain
+{
+    public interface IAccountService
+    {
+        public void OpenSavingsAccount(long accountId, long amountToDeposit);
+
+        public void OpenCurrentAccount(long accountId);
+
+        public void Withdraw(long accountId, int amountToWithdraw);
+        // throws AccountNotFoundException, WithdrawalAmountTooLargeException;
+
+        public void Deposit(long accountId, int amountToDeposit);
+        //throws AccountNotFoundException;
+    }
+}
